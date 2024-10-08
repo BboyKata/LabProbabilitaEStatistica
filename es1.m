9e04f8b103cc39f74daf15e1ac12f55e85a5a820
MATTEO BALZERANI <bboykata@balzerani.it>
2024-10-08 12:06:07 +0200

    TEST

--- es1.m
+++ es1.m
@@ -1,3 +1,16 @@
 students = 103;
 disp('The value of x is:');
-disp(students)
+disp(students);
+r = [2,4,10];
+w = [9,-6,4];
+u = [r;w];
+x = 1:2:10; 
+% ^ crea vettore che contiene elementi distanziati 2, partendo da 1, fino a
+% 10
+y = [1:2:10]
+y(2,:) = [1:2:10];
+y1 = (y')
+y1 = y1(1:2,1:2)
+u(:);
+B = magic(52);
+A = y1*y1'
